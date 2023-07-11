8323767fa3aa4cde34577c6877fd6ab7d20ebbba
Milo <milo.bascombe@ofqual.gov.uk>
2023-07-11 21:53:10 +0100

    .

--- services/basic_info_scraper.py
+++ services/basic_info_scraper.py
@@ -7,5 +7,5 @@ import urllib.parse
 from models.basic import Report, Section
 import pandas as pd
 
-# needs to be from .env
+# needs to be from .env (os.getenv('BASE_URL'))
 reports_url = "https://www.gov.uk/service-standard-reports?page="
