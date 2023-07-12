6b2a6fc54e9bccfbaeb1f42f7b5fbcc964f45bbf
Anna Horstmann <anna.horstmann@anna.horstmann's Mac>
2023-07-12 14:31:45 +0100

    Add alembic stuff.

--- alembic_files/env.py
+++ alembic_files/env.py
@@ -4,7 +4,7 @@ from sqlalchemy import engine_from_config
 from sqlalchemy import pool
 
 from alembic import context
-
+from models import Base, Report, Section
 # this is the Alembic Config object, which provides
 # access to the values within the .ini file in use.
 config = context.config
@@ -18,7 +18,7 @@ if config.config_file_name is not None:
 # for 'autogenerate' support
 # from myapp import mymodel
 # target_metadata = mymodel.Base.metadata
-target_metadata = None
+target_metadata = [Base.metadata] #, Report.metadata, Section.metadata]
 
 # other values from the config, defined by the needs of env.py,
 # can be acquired:
