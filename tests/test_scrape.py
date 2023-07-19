496bc33326fd52ed3e728344e683ee8a285f6143
Ammaar Limbada <ammaar.limbada@madetech.com>
2023-07-19 14:50:22 +0100

    fixed tests

--- tests/test_scrape.py
+++ tests/test_scrape.py
@@ -1,5 +1,5 @@
 from models.basic import Report
-from services.basic_info_scraper import get_all_reports, get_all_service_standard_links, get_all_service_standard_links_by_page, get_report_info, scrape_report_html, create_report_model
+from services.basic_info_scraper import get_reports, get_report_links, get_report_links_by_page, get_report_info, scrape_report_html, create_report_model
 import pytest
 import responses
 import requests
@@ -131,21 +131,21 @@ def test_create_report_model():
     }
     assert type(create_report_model(info_dict, url)) == Report
 
-def test_get_all_service_standard_links_by_page_returns_expected_links(mocked_report_list_page_response):
-    page_links = get_all_service_standard_links_by_page(1)
+def test_get_report_links_by_page_returns_expected_links(mocked_report_list_page_response):
+    page_links = get_report_links_by_page(1)
     links_per_page = 50
     assert len(page_links) == links_per_page
 
-def test_get_all_service_standard_links_returns_expected_links(mocked_report_list_all_response):
-    all_links = get_all_service_standard_links()
+def test_get_report_links_returns_expected_links(mocked_report_list_all_response):
+    all_links = get_report_links()
     total_link_count = 50
     assert len(all_links) == total_link_count
 
-def test_get_all_service_standard_links_returns_one(mocked_main_page_response_one):
-    all_links = get_all_service_standard_links()
+def test_get_report_links_returns_one(mocked_main_page_response_one):
+    all_links = get_report_links()
     total_link_count = 1
     assert len(all_links) == total_link_count
 
-def test_get_all_reports_returns_list(mocked_main_page_response):
-    reports_list = get_all_reports()
+def test_get_reports_returns_list(mocked_main_page_response):
+    reports_list = get_reports()
     assert type(reports_list) == list
