ced7fb36922c479ab9f6cbf5ec0bead65265227e
Ammaar Limbada <ammaar.limbada@madetech.com>
2023-07-27 15:08:18 +0100

    Fix for inserting sections into DB

--- backend/data/report_writer.py
+++ backend/data/report_writer.py
@@ -1,4 +1,4 @@
-from models.report import Base, Report
+from models.report import Base, Report, Section
 from sqlalchemy import Connection
 from sqlalchemy.dialects.postgresql import insert
 from sqlalchemy.orm import Session
@@ -11,20 +11,53 @@ def insert_entry(entry: Base, engine):
     session.close()
 
 def upsert_report(report: Report, conn: Connection):
-    statement = insert(Report.__table__).values(
-            assessment_date=report.assessment_date,
+    # Insert report
+    report_table = Report.__table__
+    statement = insert(report_table).values(
+        assessment_date=report.assessment_date,
+        overall_verdict=report.overall_verdict,
+        name=report.name,
+        url=report.url,
+        stage=report.stage
+    )
+    # Update report if conflicted with unique constraint
+    statement = statement.on_conflict_do_update(
+        constraint="report_url_key",
+        set_=dict(
             overall_verdict=report.overall_verdict,
-            name=report.name,
-            url=report.url,
-            stage=report.stage
-        ).on_conflict_do_update(
-            constraint="report_url_key",
-            set_=dict(
-                overall_verdict=report.overall_verdict,
-                stage=report.stage,
-                assessment_date=report.assessment_date
-            ))
+            stage=report.stage,
+            assessment_date=report.assessment_date
+        )
+    )
+    # Return the id of affected rows
+    statement = statement.returning(report_table.c["id"])
+
+    # Execute statement and extract report_id from result
+    for row in conn.execute(statement):
+        report_id = row[0]
         
-    conn.execute(statement)
+        # Create list of section values with report_id
+        sections = []
+        for section in report.sections:
+            sections.append(dict(
+                report_id=report_id,
+                number=section.number,
+                decision=section.decision
+            ))
+
+        # Bulk insert sections
+        if any(sections):
+
+            statement = insert(Section.__table__).values(sections)
+
+        # Update section if conflicted with unique constraint
+            statement = statement.on_conflict_do_update(
+                constraint="section_report_id_number_key",
+                set_=dict(
+                    decision=statement.excluded.decision
+                )
+            )
+        conn.execute(statement)
+
     conn.commit()
     conn.close()
