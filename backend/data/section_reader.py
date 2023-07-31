91844afa42fc41a3a97dbae895cda54921bdae82
sam fallowfield <sam.fallowfield1@gmail.com>
2023-07-31 12:05:10 +0100

    added test for the section scraper

--- backend/data/section_reader.py
+++ backend/data/section_reader.py
@@ -1,7 +1,7 @@
 from typing import List
 from fastapi_pagination import LimitOffsetPage
 from fastapi_pagination.ext.sqlalchemy import paginate
-from models.report import Section
+from models.report import Section 
 from sqlalchemy import select
 from sqlalchemy.orm import Session
 
@@ -10,4 +10,4 @@ def get_sections(id, database: Session) -> List[Section]:
 
    return database.query(Section).filter(Section.report_id == id).all()
 
-    
+
