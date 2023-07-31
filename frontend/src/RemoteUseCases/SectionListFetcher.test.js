db91526519405335521b1b2bbb95eb1cc157f8d5
jaseera@madetech.com <jaseera@madetech.com>
2023-07-31 14:18:40 +0100

    Add test

---
+++ frontend/src/RemoteUseCases/SectionListFetcher.test.js
@@ -0,0 +1,21 @@
+import axios from 'axios';
+import nock from 'nock';
+import getSectionList from './SectionListFetcher';
+axios.defaults.adapter = 'http'
+
+it('renders successfully', async () => {
+  const id = 12
+
+  const scope = nock('http://localhost:8000')
+  .get(`/reports/${id}/sections`)
+  .reply(200, [{ number: 1, decision: 'met' }]
+  , {
+    'Access-Control-Allow-Origin': '*',
+    'Content-type': 'application/json'
+  });
+
+  await getSectionList(id);
+
+  expect(scope.isDone()).toBe(true)
+  nock.cleanAll()
+});
