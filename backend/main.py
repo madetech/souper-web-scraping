c89ad6bf7d8235c5d904033df6107e130099779e
Milo <milo.bascombe@ofqual.gov.uk>
2023-08-15 22:03:23 +0100

    .

--- backend/main.py
+++ backend/main.py
@@ -36,9 +36,8 @@ app.add_middleware(
 )
 app.add_middleware(GZipMiddleware, minimum_size=1000)
 
-# .1 #If this is spammed does that mean it still performs db connection attempts
 @app.get("/scrape", status_code=201)
-def scrape_report_data(session: Session = Depends(db.get_session)): #.1
+def scrape_report_data(session: Session = Depends(db.get_session)):
     if not app.is_scraping:
         app.is_scraping = True
         report_data = scrape_reports()
