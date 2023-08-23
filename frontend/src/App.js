ffab330d7ac23404568b8237da2cec238082e79e
jaseera@madetech.com <jaseera@madetech.com>
2023-08-23 15:42:39 +0100

    Add scrape button

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,12 +1,32 @@
-import ReportList from './Components/ReportList.js';
+
+import React, { useState } from 'react';
 import Navbar from './Components/Navbar';
-import React from 'react';
+import ReportList from './Components/ReportList.js';
+import runScrape from './RemoteUseCases/RunScraper';
 
 export default function App() {
+  const [scrapeProgress, setScrapeProgress] = useState("");
+  const [openDialog, setOpenDialog] = useState(false);
+
+  const handleClickOpenDialog = async () => {
+    setOpenDialog(true);
+    setScrapeProgress(await runScrape())
+  };
+
+  const handleCloseDialog = (value) => {
+    setOpenDialog(false);
+  };
+
+
   return (
     <div className="App">
       <React.Fragment>
-        <Navbar/>
+        <Navbar 
+        onClose={handleCloseDialog} 
+        onOpen={handleClickOpenDialog} 
+        open={openDialog} 
+        progress={scrapeProgress}
+        />
         <ReportList />
       </React.Fragment>
     </div>
--- frontend/src/Components/Navbar.js
+++ frontend/src/Components/Navbar.js
@@ -1,24 +1,52 @@
-import {useRef} from 'react';
-import "../styles/main.css" ;
+import CheckCircleOutlineIcon from '@mui/icons-material/CheckCircleOutline';
+import { Typography } from '@mui/material';
+import Alert from '@mui/material/Alert';
+import Backdrop from '@mui/material/Backdrop';
+import Box from '@mui/material/Box';
+import Button from '@mui/material/Button';
+import CircularProgress from '@mui/material/CircularProgress';
+import Dialog from '@mui/material/Dialog';
+import { useRef } from 'react';
+import "../styles/main.css";
 import logo from './Navbar/logo.png';
 
-
-function Navbar() {
+export default function Navbar(props) {
   const navRef = useRef();
 
-
-  return ( 
-      <header>
-          <nav ref={navRef}>
-              <img src={logo} alt="logo" />
-              <div className="nav-links">
-                <a href="/#"> Run scrape</a>
-                <a href="/reports"> Reports</a>
-                <a href="/#">Data graph</a>
-              </div>
-          </nav>
-      </header>
+  return (
+    <header>
+      <nav ref={navRef}>
+        <img src={logo} alt="logo" />
+        {<Box className="nav-links">
+          <Button variant="text" sx={{ color: 'black', textTransform: "none" }} onClick={props.onOpen}>
+            Run Scrape
+          </Button>
+          
+          {props.progress == "201" ?
+            <Dialog onClose={props.onClose}
+              open={props.open}
+            >
+              <Alert
+                iconMapping={{
+                  success: <CheckCircleOutlineIcon fontSize="inherit" />,
+                }}
+              >
+                All records scrapped successfully
+              </Alert>
+            </Dialog>
+            :
+            <Backdrop
+              sx={{ color: '#fff', zIndex: (theme) => theme.zIndex.drawer + 1 }}
+              open={props.open}
+              onClick={props.onClose}
+            >
+              <CircularProgress style={{ 'color': 'darkgreen' }} />
+              <Typography sx={{ pl: 2 }}>Scrapping in progress...</Typography>
+            </Backdrop>
+          }
+        </Box>}
+      </nav>
+    </header>
   );
 }
 
-export default Navbar;
