86896a2d61c1158316b638f1b8c08e30b0933544
Anna Horstmann <anna.horstmann@anna.horstmann's Mac>
2023-10-02 08:17:21 +0100

    Remove comments from feedback_reader.

--- backend/data/feedback_reader.py
+++ backend/data/feedback_reader.py
@@ -6,16 +6,4 @@ from sqlalchemy.orm import Session
 
 def get_feedback(id, session: Session) -> List[Feedback]:
     with session:
-        #return id
         return session.query(Feedback).filter(Feedback.section_id == id).all()
-        #return session.query(Feedback).all()
-
-
-# def get_feedback(id, session: Session) -> List[Feedback]:
-#     with session:
-#         query = session.query(Feedback)
-#         return filter_feedback(query, id)
-    
-
-# def filter_feedback(query, id) -> List[Feedback]:
-#     return query.filter_by(Feedback.section_id == id).all()
