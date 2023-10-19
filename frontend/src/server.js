b4be1414584a535beb0bb65da0741858a06a6439
sam fallowfield <sam.fallowfield1@gmail.com>
2023-10-19 13:37:05 +0100

    create serverside functionality

--- frontend/src/server.js
+++ frontend/src/server.js
@@ -3,8 +3,10 @@ const express = require('express')
 var cors = require('cors')
 const app = express()
 const path = require('path');
-const hostname = process.env.BACKEND_HOST;
-const port = process.env.BACKEND_PORT;
+// const hostname = process.env.BACKEND_HOST;
+// const port = process.env.BACKEND_PORT;
+const axios = require('axios')
+var backend_url = process.env.REACT_APP_BACKEND_URL
 
 var corsOptions = {
     origin: `${process.env.REACT_APP_BACKEND_URL}`
@@ -17,11 +19,36 @@ app.use(express.static(path.join(process.cwd(), 'build')));
 
 app.get('/', function (_, res, _) {
     res.sendFile('index.html');
+
   });
 
+app.get('/reports', function(_, res) {
+  axios.get(`${backend_url}/reports`)
+  .then(backend_res => backend_res.status === 200 ? res.json(backend_res.data.items) : res.status(backend_res.status))
+  .catch(err => err);
+});
+
+app.get(`/reports/:id/sections`, function(req,res) {
+  var id = req.params['id']
+  axios.get(`/reports/${id}/sections`)
+  .then(backend_res => backend_res.status === 200 ? res.json(backend_res.data) : res.status(backend_res.status))
+  .catch(err => err);
+});
+
+app.get(`/sections/:id/feedback`, function(req,res) {
+  var id = req.params['id']
+  axios.get(`sections/${id}/feedback`)
+  .then(backend_res => backend_res.status === 200 ? res.json(backend_res.data) : res.status(backend_res.status))
+  .catch(err => err);
+
+})
+
+app.post('/scrape', function(req, res) {
+  axios.post(`${backend_url}/scrape`)
+  .then(backend_res => res.status(backend_res.status))
+  .catch(err => err);
+  
+});
 
 app.listen(3000);
 
-// app.listen(3000, () => {
-//   console.log("Your app is listening on port " + listener.address().port);
-// });
