e20ce6134a4c62744ec63cab87e1291f306ca05d
sam fallowfield <sam.fallowfield1@gmail.com>
2023-10-20 10:08:29 +0100

    revert issues from merge

--- frontend/src/server.js
+++ frontend/src/server.js
@@ -21,24 +21,26 @@ app.get('/', function (_, res, _) {
 
 app.get('/reports', function(_, res) {
   axios.get(`${backend_url}/reports`)
-  .then(backend_res => backend_res.status === 200 ? res.json(backend_res.data.items) : res.status(backend_res.status))
-  .catch(err => err);
+  .then(backend_res => backend_res.status === 200 ? res.send(backend_res.data) : res.sendStatus(backend_res.status))
+  .catch(next);
 });
+
+
 
 app.get(`/reports/:id/sections`, function(req,res) {
   var id = req.params['id']
-  axios.get(`/reports/${id}/sections`)
-  .then(backend_res => backend_res.status === 200 ? res.json(backend_res.data) : res.status(backend_res.status))
-  .catch(err => err);
-});
+  axios.get(`${backend_url}/reports/${id}/sections`)
+  .then(backend_res => backend_res.status === 200 ? res.send(backend_res.data) : res.sendStatus(backend_res.status))
+  .catch(next);
+})
 
 app.get(`/sections/:id/feedback`, function(req,res) {
   var id = req.params['id']
-  axios.get(`sections/${id}/feedback`)
-  .then(backend_res => backend_res.status === 200 ? res.json(backend_res.data) : res.status(backend_res.status))
-  .catch(err => err);
-
-})
+  axios.get(`${backend_url}/sections/${id}/feedback`)
+  .then(backend_res => backend_res.status === 200 ? res.send(backend_res.data) : res.sendStatus(backend_res.status))
+  .catch(next);
+  
+  })
 
 app.post('/scrape', function(req, res) {
   axios.post(`${backend_url}/scrape`)
@@ -62,17 +64,17 @@ app.get('/reports/:id/sections', function (req, res, next) {
 })
 
 app.get(`/sections/:id/feedback`, function (req, res, next) {
-var id  = req.params['id']
-axios.get(`${backend_url}/sections/${id}/feedback`)
-.then(backend_res => backend_res.status === 200 ? res.send(backend_res.data) : res.sendStatus(backend_res.status))
-.catch(next);
+  var id  = req.params['id']
+  axios.get(`${backend_url}/sections/${id}/feedback`)
+  .then(backend_res => backend_res.status === 200 ? res.send(backend_res.data) : res.sendStatus(backend_res.status))
+  .catch(next);
 })
 
 app.get('/scrape', function(req, res, next) {
   axios.get(`${backend_url}/scrape`)
   .then(backend_res => res.sendStatus(backend_res.status))
   .catch(next);
-})
+  })
 
 app.use((err, req, res, next) => {
   console.log(err);
