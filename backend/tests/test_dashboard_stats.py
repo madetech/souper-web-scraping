21fb059d7f72832b82241b8d728592f523dd906f
Jarec <jarec.basham@madetech.com>
2023-12-19 13:43:56 +0000

    Update test_dashboard_stats.py

--- backend/tests/test_dashboard_stats.py
+++ backend/tests/test_dashboard_stats.py
@@ -5,9 +5,10 @@ from backend.data import dashboard
 
 
 class DashboardStatsTest(unittest.TestCase):
-    @patch("backend.data.session.query", return_value='[]')
-    def test_get_counts(self):
+    @patch('data.feedback_reader.Session')
+    def test_get_counts(self, mock_session):
+        mock_session.query.return_value.filter.return_value.get_counts.return_value = ""
         expected = [[["Stage", "Count"],["Alpha", 250],["Beta", 420],["Live", 290]], [["Stage", "Count"],["Alpha", 37],["Beta", 60],["Live", 70]]]
-        result = dashboard.get_counts(None)
+        result = dashboard.get_counts(mock_session)
         self.assertEqual(result, expected)
         
