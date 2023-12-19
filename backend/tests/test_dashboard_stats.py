adfb2ca23c8979d8e96d585f54784c0e5dcfd8f6
Jarec <jarec.basham@madetech.com>
2023-12-19 19:13:12 +0000

    Fixed get_counts test mock

--- backend/tests/test_dashboard_stats.py
+++ backend/tests/test_dashboard_stats.py
@@ -1,5 +1,5 @@
 import unittest
-from unittest.mock import patch
+from unittest.mock import Mock, patch
 from models.report import Report
 
 from backend.data import dashboard
@@ -8,9 +8,17 @@ from backend.data import dashboard
 class DashboardStatsTest(unittest.TestCase):
     @patch('data.dashboard.Session')
     def test_get_counts(self, mock_session):
-        query = mock_session.query(Report).filter(Report.stage == 'Alpha')
-        mock_session.query.return_value = query
+        mock_query = Mock()
+        mock_query.filter().count.return_value = 42
+        mock_session_instance = mock_session.return_value
+        mock_session_instance.query.return_value = mock_query
         expected = [[["Stage", "Count"],["Alpha", 250],["Beta", 420],["Live", 290]], [["Stage", "Count"],["Alpha", 37],["Beta", 60],["Live", 70]]]
-        result = dashboard.get_counts(mock_session)
+        result = dashboard.get_counts(mock_session_instance)
+
         self.assertEqual(result, expected)
+
+        mock_session.assert_called_once_with()
+        mock_session_instance.query.assert_called_once_with(Report)
+        mock_query.filter.assert_called_once_with(Report.stage == "Alpha")
+        mock_query.filter().count.assert_called_once_with()
         
