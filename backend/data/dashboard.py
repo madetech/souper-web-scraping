b995c6ad2d2468497adfb92b31aa9bdc0b096dd5
Jarec <jarec.basham@madetech.com>
2023-12-19 16:02:24 +0000

    Setup of query mock

--- backend/data/dashboard.py
+++ backend/data/dashboard.py
@@ -1,16 +1,16 @@
-
+from models.report import Report
 from sqlalchemy.orm import Session
 
 
 def get_counts(session: Session):
     with session:
-        resultSet = session.query().all()
+        resultSet = session.query(Report).filter(Report.stage == "Alpha").count() # TODO: Fix this to be the correct query: Just pull count of one element first
         print(resultSet)
         results = __format_count_output(resultSet)
     return results
 
-def __format_count_output(results):
+def __format_count_output(resultSet):
     """ Should look like this
     [[["Stage", "Count"],["Alpha", 250],["Beta", 420],["Live", 290]], [["Stage", "Count"],["Alpha", 37],["Beta", 60],["Live", 70]]]
         """
-    return ""
+    return resultSet
--- backend/tests/test_dashboard_stats.py
+++ backend/tests/test_dashboard_stats.py
@@ -1,13 +1,15 @@
 import unittest
 from unittest.mock import patch
+from models.report import Report
 
 from backend.data import dashboard
 
 
 class DashboardStatsTest(unittest.TestCase):
-    @patch('data.feedback_reader.Session')
+    @patch('data.dashboard.Session')
     def test_get_counts(self, mock_session):
-        mock_session.query.return_value.filter.return_value.get_counts.return_value = ""
+        query = mock_session.query(Report).filter(Report.stage == 'Alpha')
+        mock_session.query.return_value = query
         expected = [[["Stage", "Count"],["Alpha", 250],["Beta", 420],["Live", 290]], [["Stage", "Count"],["Alpha", 37],["Beta", 60],["Live", 70]]]
         result = dashboard.get_counts(mock_session)
         self.assertEqual(result, expected)
