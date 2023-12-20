c2df9b0d9d89686ba143e7625c27b6ebdd92a2ba
Jarec <jarec.basham@madetech.com>
2023-12-20 11:11:46 +0000

    Fixed mock

--- backend/data/dashboard.py
+++ backend/data/dashboard.py
@@ -7,16 +7,14 @@ def get_counts(session: Session):
     with session:
             result_set = (
             session.query(Report.stage, Report.overall_verdict, func.count().label('count'))
-            .group_by(Report.stage, Report.overall_verdict)
             .all()
         )
         
-    for result in result_set:
-        print(f"Stage: {result.stage}, Type: {result.overall_verdict}, Count: {result.count}")
-    return result_set
+    return __format_count_output(result_set)
 
-def __format_count_output(resultSet):
+def __format_count_output(result_set):
     """ Should look like this
     [[["Stage", "Count"],["Alpha", 250],["Beta", 420],["Live", 290]], [["Stage", "Count"],["Alpha", 37],["Beta", 60],["Live", 70]]]
         """
-    return resultSet
+    print(result_set)
+    return result_set
--- backend/tests/test_dashboard_stats.py
+++ backend/tests/test_dashboard_stats.py
@@ -9,23 +9,18 @@ from backend.data import dashboard
 class DashboardStatsTest(unittest.TestCase):
     @patch('data.dashboard.Session')
     def test_get_counts(self, mock_session):
-        MockRecord = namedtuple('MockRecord', ['stage', 'result', 'count'])
-        mock_data = [
-            MockRecord(stage='alpha', result='met', count=5),
-            MockRecord(stage='alpha', result='not met', count=2),
-            MockRecord(stage='beta', result='met', count=15),
-            ]
-        mock_query = Mock()
-        mock_query.filter().count.return_value = mock_data
-        mock_session_instance = mock_session.return_value
-        mock_session_instance.query.return_value = mock_query
-        expected = [[["Stage", "Count"],["Alpha", 250],["Beta", 420],["Live", 290]], [["Stage", "Count"],["Alpha", 37],["Beta", 60],["Live", 70]]]
-        result = dashboard.get_counts(mock_session_instance)
+        MockResult = namedtuple('MockResult', ['stage', 'overall_verdict', 'count'])
+        mock_result_set = [
+            MockResult(stage='Alpha', overall_verdict='Met', count=20),
+            MockResult(stage='Alpha', overall_verdict='Not Met', count=5),
+            MockResult(stage='Beta', overall_verdict='Met', count=10),
+            MockResult(stage='Beta', overall_verdict='Not Met', count=8),
+            MockResult(stage='Live', overall_verdict='Met', count=35),
+            MockResult(stage='Live', overall_verdict='Not Met', count=7),
+        ]
+        mock_session.query.return_value.all.return_value = mock_result_set
+        expected = [[["Stage", "Count"],["Alpha", 20],["Beta", 10],["Live", 35]], [["Stage", "Count"],["Alpha", 5],["Beta", 8],["Live", 7]]]
+        result = dashboard.get_counts(mock_session)
 
         self.assertEqual(result, expected)
-
-        mock_session.assert_called_once_with()
-        mock_session_instance.query.assert_called_once_with(Report)
-        mock_query.filter.assert_called_once_with(Report.stage == "Alpha")
-        mock_query.filter().count.assert_called_once_with()
         
