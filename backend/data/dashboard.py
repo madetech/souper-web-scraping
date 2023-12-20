1ff793b29a037b5c193d47766104d2af99891217
Jarec <jarec.basham@madetech.com>
2023-12-20 13:26:26 +0000

    Sorted result set

--- backend/data/dashboard.py
+++ backend/data/dashboard.py
@@ -16,5 +16,9 @@ def __format_count_output(result_set):
     """ Should look like this
     [[["Stage", "Count"],["Alpha", 250],["Beta", 420],["Live", 290]], [["Stage", "Count"],["Alpha", 37],["Beta", 60],["Live", 70]]]
         """
-    print(result_set)
-    return result_set
+    sorted_result_set = sorted(
+        result_set,
+        key=lambda result: (result.overall_verdict, result.stage)
+        )
+    print(sorted_result_set)
+    return sorted_result_set
