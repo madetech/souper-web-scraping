2d831604408fee790e8a5ef1ef671ac21833b767
Jarec <jarec.basham@madetech.com>
2023-12-22 14:31:12 +0000

    Minor modal changes

--- frontend/src/Components/ChartModal.js
+++ frontend/src/Components/ChartModal.js
@@ -1,11 +1,16 @@
 import React, { useEffect, useState } from 'react';
 import Modalhelper from '../Helpers/ModalHelper';
+import Box from '@mui/material/Box';
+import { modalStyle } from '../Helpers/ModalStyle';
 import { Chart } from "react-google-charts";
 
 export default function ChartModal() {
   return (
-    <Modalhelper>
-      <Chart
+    <Modalhelper 
+    open>
+      <Box sx={modalStyle}>
+        <h1>Sample text</h1>
+      {/* <Chart
         chartType="ComboChart"
         width="100%"
         height="400px"
@@ -21,7 +26,8 @@ export default function ChartModal() {
           hAxis: { title: "Stage" },
           seriesType: "bars",
         }}
-      />
+      /> */}
+      </Box>
     </Modalhelper>
   )
 }
